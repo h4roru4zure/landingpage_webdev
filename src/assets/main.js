e9c987871ec76c752dfcb2c848b37d239abdce05
h4roru 4zure <h4roru4zure@outlook.com>
2023-01-28 17:16:10 -0300

    version 0.02 de opbe.online vamos avanzando

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,7 +1,8 @@
-const fetch = require('node-fetch');
-//import fetch from 'node-fetch';
 
-const url = 'https://bb-finance.p.rapidapi.com/stories/list?id=usdjpy&template=CURRENCY';
+const API = 'https://bb-finance.p.rapidapi.com/stories/list?id=usdjpy&template=CURRENCY';
+
+ const content = null || document.getElementById('content');
+//const fetch = require('node-fetch');
 
 const options = {
   method: 'GET',
@@ -11,7 +12,37 @@ const options = {
   }
 };
 
-fetch(url, options)
-	.then(res => res.json())
-	.then(json => console.log(json))
-	.catch(err => console.error('error:' + err));
+async function fetchData(API) {
+  const response = await fetch(API, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const noticias = await fetchData(API);
+    let view = `
+    ${noticias.stories.map (noticia=> `
+      <div class="group relative">
+        <div
+        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+        <a href="${noticia.longURL}" ><img src="${noticia.thumbnailImage}"  class="w-full" /> </a>
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+             ${noticia.title}
+            
+          </h3>
+        </div>
+      </div>
+    `).slice(0,8).join('')}
+
+    `;
+    content.innerHTML = view;
+  } catch (error) {
+  
+    console.log(error);
+    window.alert(error.message);
+  }
+})();
